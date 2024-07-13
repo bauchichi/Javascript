222ebd93a8b71d5e708a72cc63f6a5e7925934e6
bauchichi <159606081+bauchichi@users.noreply.github.com>
2024-07-13 14:30:03 +0000

    fghjkl

---
+++ 01_basics/0_1variables.js
@@ -0,0 +1,13 @@
+const accountID =  143523
+let accoutEmail =  "hitesh@gmailgoogle.po"
+var accountPassword = "12345"
+acoountCity = "Jaipur"
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21211212"
+accountCity = "Bengaluru"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/2_dtypes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log(3+3);
+
+//bigint
+//standalone value = NULL ((it is an object))
+//symbol
+//undefined (symbol hai)(iska data type undefined hi ata hai )
+
+
+// object
+
+console.log(typeof undefined);//(undefined)
+console.log(typeof null);//(object)
