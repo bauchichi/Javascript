14d1efd9a88c0f876b15a87a3b9ddfb828276b65
bauchichi <159606081+bauchichi@users.noreply.github.com>
2024-07-14 04:11:20 +0000

    conversion

---
+++ 01_basics/3_conversion_op.js
@@ -0,0 +1,18 @@
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+ 
+let a=1
+let b =  Boolean(a)
+console.log(b);
+
+//"hitesh" => true  "" => false
+
+let c = 34
+
+let d = string(c)
+console.log(d)
+console.log(typeof(d))
