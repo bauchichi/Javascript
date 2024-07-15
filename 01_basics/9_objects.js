bb87c1622fa73549b70f3778ab137a530e68c569
bauchichi <159606081+bauchichi@users.noreply.github.com>
2024-07-15 11:03:49 +0000

    objects2

--- 01_basics/9_objects.js
+++ 01_basics/9_objects.js
@@ -6,11 +6,11 @@
 
 // const sym1 = Symbol("key1")
 
-const a = {
-    name:"hitesh",
-    "full name" : "kaka",
-    [sym1]:"lalaondafloor"
-}
+// const a = {
+//     name:"hitesh",
+//     "full name" : "kaka",
+//     [sym1]:"lalaondafloor"
+// }
 
 
 // console.log(typeof(a[sym1]))
@@ -19,12 +19,46 @@ const a = {
 // console.log(a)
 
 
-a.greetings = function(){
-    console.log("hello js user")
-}
-
-console.log(a.greetings)
-
 
 
 // ==============================last part dekhna hai objects ka ====================
+
+
+const reguser = {
+    email:"lalaondaboat",
+    fullname:{
+        userfullname:"heuheu",
+        sidename:"chahca"
+    }
+}
+
+console.log(reguser.fullname.sidename)
+
+
+const a ={ 1:"a", 2:"b"}
+const b ={ 3:"a", 4:"k"}
+
+const c = {...a, ...b}
+console.log(c)
+
+const users  = [
+    {
+        id:1,
+        email:"heuheu",
+    },
+    {
+        tame:2,
+        name:"chame",
+    }
+]
+
+
+users[1].email
+console.log(id)
+
+console.log(Object.keys(id))//galat hogya code halka sa 
+
+console.log(Object.entries())
+
+console.log(users.hasOwnProperty)
+
