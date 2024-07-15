2f6b6dd29ae71e90c51c79ea9cf625d34d6f6f19
bauchichi <159606081+bauchichi@users.noreply.github.com>
2024-07-15 15:16:26 +0000

    functions

--- 01_basics/10_functions.js
+++ 01_basics/10_functions.js
@@ -23,4 +23,27 @@ function loginusermessage(username){
 
 console.log(loginusermessage("hitesh"))
 
+function calculatecartprice(val1,val2, ...num1){
+    return num1
+}
 
+console.log(calculatecartprice(200,400,500,2000))
+
+const user = {
+    username:"hetish",
+    price:560
+}
+
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+
+const newarr1 = [200,499,3213]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue([200,300,400]))
