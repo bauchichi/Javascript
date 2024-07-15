43d9f069269caa654c9b116b4d9ab4362fd5567b
bauchichi <159606081+bauchichi@users.noreply.github.com>
2024-07-15 12:03:31 +0000

    functions

---
+++ 01_basics/10_functions.js
@@ -0,0 +1,26 @@
+
+// function sayMyName(){
+
+//  console.log("q")
+//   console.log("d")
+//  console.log("k")
+//  console.log("l")
+//  console.log("o")
+// }
+
+// //sayMyName()
+
+// function ak(n1,n2){
+//     console.log(n1+n2)
+// }
+
+// ak(32,45)
+
+
+function loginusermessage(username){
+    return`${username} just logged in `
+}
+
+console.log(loginusermessage("hitesh"))
+
+
