5b835a901c2e3878ae2a4c40fb69c48b827d515f
bauchichi <159606081+bauchichi@users.noreply.github.com>
2024-07-16 09:37:51 +0000

    woo

---
+++ 01_basics/14_Iterations.js
@@ -0,0 +1,18 @@
+
+// for
+
+// for (let index = 0; index <= 10; index++){
+//     const element = index;
+//     console.log(element);
+// }
+
+//console.log(element);
+
+// 
+
+let arr1 = ["a", "b", "c"]
+console.log(arr1.length)
+for(let index = 0; index < arr1.length; index++){
+    const a = arr1[index];
+    console.log(a);
+}
